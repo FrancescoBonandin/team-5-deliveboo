93a9a9859270dc478818bdb35261a571f39d69d1
Giuliano Blas <giuliano.blass@gmail.com>
2023-10-11 18:05:20 +0200

    create : dishes migration

---
+++ database/migrations/2023_10_11_155044_create_dishes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('name', 70);
+
+            $table->text('ingredients');
+            
+            $table->text('description');
+
+            $table->unsignedDecimal('price', 4, 2);
+            
+            $table->boolean('available')->default(true);
+            
+            $table->sring('image', 2048)->nullable();
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
