2189865ac9dad9e481870ea55a5b10b07cefc4d6
Giuliano Blas <giuliano.blass@gmail.com>
2023-10-12 18:13:38 +0200

    create : Category Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::withoutForeignKeyConstraints(function () {
+
+            Category::truncate();
+
+        });
+
+        $restaurant_categories = [ 
+
+            'italiano',
+
+            'internazionale',
+
+            'cinese', 
+
+            'giapponese',
+
+            'messicano', 
+
+            'indiano',
+
+            'pesce', 
+
+            'carne',
+                
+            'pizza',
+
+            'hamburger',
+               
+        ];
+
+        for ($i=0; $i < 10; $i++) { 
+
+            $random_categories = $restaurant_categories[$i];
+
+            Category::create([
+
+                'category_name' => $random_categories,
+                
+            ]);
+            
+        }
+    }
+}
