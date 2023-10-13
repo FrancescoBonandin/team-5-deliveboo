599d2114f3971e81eebdc523153ef82fd7a5cf12
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-10-13 19:37:29 +0200

    EDIT:  modificata la view dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,15 @@
                     <h1 class="text-center text-success">
                         Sei loggato!
                     </h1>
+
+                    <h1>
+                        {{ Auth()->user()->name }}
+                    </h1>
+
+                    <h1>
+                        {{Auth()->user()->restaurant->restaurant_name}}
+                    </h1>
+
                     <br>
                     La dashboard è una pagina privata (protetta dal middleware)
                 </div>
