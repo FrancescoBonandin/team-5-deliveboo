f78d903520902d8e1af0b6645dc386e42d75716f
EnricoValente <enricovalente42@gmail.com>
2023-10-13 17:12:47 +0200

    create: DishController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DishController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+Route::resource('dishes', DishController::class)->middleware(['auth', 'verified'])->except(['index']);
 
 require __DIR__.'/auth.php' ;
