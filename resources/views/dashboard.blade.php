59ffadea01df640f1ee87517a3b27185125ee93f
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-10-14 14:45:42 +0200

    Edit: modificata dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     </h1>
 
                     <h3>
-                        Questa è la tua dashboard da cui puoi controllare tutti i tuoi piatti e i tuoi ordini
+                        Questa è la tua dashboard da cui puoi controllare tutti i tuoi ordini, i tuoi piatti e perfino le statistiche
                     </h3>
 
 
@@ -26,98 +26,18 @@
         <div class="row">
                 
             <div class="col-4">
-                
-                @forelse (Auth()->user()->restaurant->orders as $order)
 
-                    <div class="card">
-                        <a href="{{route('orders.view')}}">
+                <div class="card">
+                    <a href="{{route('orders.view')}}">
 
-                            <div class='card-title row'>
-           
-                                <h2 class='col'>
-                                   Cliente: {{$order->name}} {{$order->last_name}}
-                                </h2>
-           
-                                <h2 class='col'>
-                                   Num.Ordine: {{$order->id}}
-                                </h2>
+                        <h2 class="card-title">
                             
-                            </div>
+                            Orders
                             
-                        </a>
-       
-                        <div class='card-body'>
-       
-                           @forelse ($order->dishes as $dish)
-                               <div class='card'>
-                                    <div class="card-body row">
-       
-                                        <div class="col-2">
-                                           <img class="img-fluid" src="{{$dish->image}}" alt="">
-                                        </div>
-       
-                                        <div class="col">
-       
-                                            <h3>
-       
-                                                <span>
-                                                   
-                                                   {{$dish->name}} 
-       
-                                                </span>
-                                           
-                                                <span>
-                                                   x {{$dish->pivot->quantity}}
-                                                </span>
-       
-                                            </h3>
-       
-                                            <h4>
-                                               {{$dish->ingredients}}
-                                            </h4>
-       
-                                            <h4>
-                                               {{$dish->description}}
-                                            </h4>
-       
-                                            <h4>
-       
-                                               Partial Price:
-                                               {{ str_replace('.',',', $dish->price*$dish->pivot->quantity)}}€
-       
-                                            </h4>
-       
-                                        </div>
-       
-                                    </div>
-                                </div>
-                            @empty
-                            nessun piatto associato
-                            @endforelse
-                           
-                            <div class="row">
-       
-                               <h4 class='col '>
-                                   Address:{{$order->address}}
-                               </h4>
-       
-                               <h4 class='col '>
-                                   Total Price:{{str_replace('.',',',$order->total_price)}}€
-                               </h4>
-       
-                            </div>
-                        </div>
-                    </div>
-                    
-                    @empty
-        
-                        <h2>
-                            Nessun ordine   
                         </h2>
-        
-                @endforelse
-
-                
+                            
+                    </a>
+                </div>
 
             </div>
             
