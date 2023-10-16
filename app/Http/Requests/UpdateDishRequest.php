10a31780b14e4dc887a5dd283edf12fbb29d8476
EnricoValente <enricovalente42@gmail.com>
2023-10-16 15:04:46 +0200

    edit: minor-fix request

--- app/Http/Requests/UpdateDishRequest.php
+++ app/Http/Requests/UpdateDishRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateDishRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateDishRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
