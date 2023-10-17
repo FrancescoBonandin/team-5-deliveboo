234fd4ecd29444b0055e7a93175fc906a5d4bc26
Giuliano Blas <giuliano.blass@gmail.com>
2023-10-18 00:18:15 +0200

    add: style to restaurant-dishes view

--- resources/views/admin/dishes/restaurant-dishes.blade.php
+++ resources/views/admin/dishes/restaurant-dishes.blade.php
@@ -4,79 +4,158 @@
 
 @section('main-content')
 
-<div class="row">
-    <div class="card">  
-        <div class="col">
-            <h1 class="card-title text-center">
-            I tuoi piatti
-            </h1>
+<div class="w-100 position-relative ">
+
+    <div class="d-flex  justify-content-between align-items-center">
+
+        <div class="btn btn-primary my-4">
+
+            <a class="text-reset text-decoration-none" href="{{ route('dashboard') }}">indietro</a>
+    
+        </div>
+    
+        <div class="my-4">
+    
+            <a class="btn btn-success" href="{{ route('dishes.create')}}">+</a>
+    
         </div>
 
-        <!-- Bottone del create -->
-        <div class="col text-end">
-            <a href="{{route('dishes.create')}}" class="btn btn-primary mb-2">Aggiungi un nuovo piatto</a>
-        </div>
     </div>
-</div>
 
+    <div class="row aling-item-center display-dishes-container light-bg-card p-2 mx-3 mb-4 mt-3 custom-shadow position-relative">
 
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
+        @forelse (Auth()->user()->restaurant->dishes as $dish)
+            
+        <div class="col-12 primary-bg-card my-2 d-flex justify-content-center align-item-center flex-wrap">
+            
+            <div class="col-lg-8 col-md-12 my-3 p-2 light-bg-card">
 
-        <!-- Lista dei piatti associati al ristorante -->
-        @forelse(Auth()->user()->restaurant->dishes as $dish)
-        <h5 class="card-title"> {{$dish->name}} </h5>
-        <img src="{{asset('/storage/'.$dish->image)}}" alt="" class="w-50">
+                <div class="row">
 
-        <!-- Bottone di visualizzazione (show) -->
-        <div class="card-body">
-        <a href="{{ route('dishes.show',['dish'=>$dish->id])}}"  class="btn btn-info">Vedi i dettagli</a>
+                    <div class="col-lg-4 col-md-3 col-sm-12 p-3">
 
-        <!-- Bottone di modifica (edit) -->
-        <a href="{{ route('dishes.edit',['dish'=>$dish->id])}}"  class="btn btn-warning">Modifica</a>
+                        <h5 class="card-title text-capitalize"> {{$dish->name}}</h5>
         
-        <!-- Bottone di eliminazione con modale-->
-            <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                Cancella
-            </button>
-            <div class="modal fade" id="deleteModal" tabindex="-1">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title"> Sei sicuro? </h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class=" text-danger">
-                                <div class=" text-warning">
-                                    Sei sicuro di voler cancellare il piatto? 
-                                </div>
-                                <div class=" text-danger">
-                                    (questa operazione sarà irreversibile)
-                                </div>
-                            </div>
-                        </div>
-                            <form  action="{{route('dishes.destroy',['dish'=>$dish->id])}}" method="POST">
-                            @csrf
-                            @method('Delete')
-                                <div class="modal-footer">
-                                    <button type='submit' class="col-3 btn btn-danger">
-                                        Cancella
-                                    </button>
-                                    <button type="button" class="col-3 btn btn-success" data-bs-dismiss="modal">
-                                        Annulla
-                                    </button>
-                                </div>
-                            </form>
                     </div>
+        
+                    <div class="col-lg-4 col-md-3 col-sm-12 p-3 align-self-center ">
+
+                        <h6 class="card-title text-uppercase">descrizione :</h6>
+        
+                        <p class="">{{$dish->description}}</p>
+        
+                    </div>
+        
+                    <div class="col-lg-4 ms-md-auto col-lg-2 col-md-4 col-sm">
+        
+                        <img src="{{asset('/storage/'.$dish->image)}}" alt="" class="rounded w-100">
+        
+                    </div>
+
                 </div>
+
             </div>
+
+            <div class="col-lg-4 col-md-12 d-flex justify-content-center">
+
+                <div class="d-flex p-2 justify-content-center flex-wrap ">
+
+                    <div class="m-2 flex-shrink-1 align-self-center">
+
+                        <a href="{{ route('dishes.show',['dish'=>$dish->id])}}"  class="btn btn-success">dettagli</a>
+
+                    </div>
+
+                    <div class="m-2 align-self-center">
+
+                        <a href="{{ route('dishes.edit',['dish'=>$dish->id])}}"  class="btn btn-warning">Modifica</a>
+
+                    </div>
+
+                    <div class="m-2 align-self-center justify-self-center">
+
+                        <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                            Cancella
+                        </button>
+
+                        <div class="modal fade" id="deleteModal" tabindex="-1">
+
+                            <div class="modal-dialog">
+
+                                <div class="modal-content">
+
+                                    <div class="modal-header">
+
+                                        <h5 class="modal-title"> Sei sicuro? </h5>
+
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                                    </div>
+
+                                    <div class="modal-body">
+
+                                        <div class="">
+
+                                            <div class="">
+
+                                                Sei sicuro di voler cancellare il piatto? 
+
+                                            </div>
+
+                                            <div class=" text-danger">
+
+                                                (questa operazione sarà irreversibile)
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                    <form  action="{{route('dishes.destroy',['dish'=>$dish->id])}}" method="POST">
+
+                                    @csrf
+
+                                    @method('Delete')
+
+                                        <div class="modal-footer">
+
+                                            <button type='submit' class="col-3 btn btn-danger">
+
+                                                Cancella
+
+                                            </button>
+
+                                            <button type="button" class="col-3 btn btn-success" data-bs-dismiss="modal">
+
+                                                Annulla
+
+                                            </button>
+
+                                        </div>
+
+                                    </form>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
         @empty
-        Nessun piatto presente
+            
         @endforelse
-      </div>
+
     </div>
-  </div>
+
 </div>
+
 @endsection
