1db7e9b4870280a5600c0a1e7331bb88cfa57048
Giuliano Blas <giuliano.blass@gmail.com>
2023-10-18 16:10:13 +0200

    add : dishes & categories in RestaurantController

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+
 use App\Models\Restaurant;
 
 class RestaurantController extends Controller
 {
     public function index() {
-        $restaurants = Restaurant::all();
+
+        $restaurants = Restaurant::with('dishes', 'categories')->get();
+
         return response()->json([
+
             'restaurants'=>$restaurants
+
         ]);
     }
 }
