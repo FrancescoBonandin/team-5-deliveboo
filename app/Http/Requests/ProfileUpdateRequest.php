c085d1e952780f818670a2a9994700b8e6d29dd0
EnricoValente <enricovalente42@gmail.com>
2023-10-18 16:06:59 +0200

    edit: minor fix Profile

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,7 +16,7 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
+            'name' => ['string', 'max:255', 'regex:/^[A-Za-z]+$/'],
             'email' => ['email', 'max:319', Rule::unique(User::class)->ignore($this->user()->id)],
             'restaurant_name'=>['required', 'max:255'],
             'address'=>['required', 'max:255'],
@@ -31,8 +31,7 @@ class ProfileUpdateRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'name' =>'Il campo Nome è obbligatorio.',
-            'email.required' => 'Il campo Email è obbligatorio.',
+            'name' =>'Il campo Nome non può essere un numero e non può superare i 255 caratteri.',
             'email.email' => 'Il campo Email deve essere un indirizzo email valido.',
             'email.unique' => 'L\'indirizzo email specificato è già in uso.',
             'restaurant_name'=>'Il campo Nome del ristorante è obbligatorio.',
@@ -42,6 +41,7 @@ class ProfileUpdateRequest extends FormRequest
             'p_iva.min' => 'Il campo Partita IVA deve contenere almeno 11 caratteri.',
             'p_iva.max' => 'Il campo Partita IVA non può superare 11 caratteri.',
             'categories.*'=>'Una delle categorie selezionate non esiste nel sistema.',
+            
             
         ];
     }
